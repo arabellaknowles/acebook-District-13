1ac18d5c094739778c8545a0c490ee8a036f14d5
Cameron Logie <cameronlogie24@gmail.com>
2020-11-23 16:40:32 +0000

    added update route

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class PostsController < ApiController
-      before_action :check_basic_auth
+      before_action :check_basic_auth, :find_post
 
       def index
         @posts = Post.order(created_at: :desc)
@@ -10,19 +10,20 @@ module Api
         @post = Post.find(params[:id])
       end
       def create
-        p post_params["message"]
-        p @current_user.id
         @post = Post.create(message: post_params["message"], user_id: @current_user.id)
-        
-        
-        # if @post
-        #   render json: @post
-        # else
-        #   render error: { error: 'Unable to create post.' }, status: 400
-        # end
+        if @post
+          render json: @post
+        else
+          render error: { error: 'Unable to create post.' }, status: 400
+        end
       end
       def update
-
+        if @post
+          @post.update(post_params) 
+          render json: { message: 'Fact successfully updated' }, status: 200
+        else
+          render error: { error: 'Unable to update fact' }, status: 400
+        end
       end
 
 
@@ -31,6 +32,10 @@ module Api
       def post_params
         params.require(:message)
         params.permit(:message)
+      end
+
+      def find_post
+        @post = Post.find(params[:id])
       end
     end
   end
