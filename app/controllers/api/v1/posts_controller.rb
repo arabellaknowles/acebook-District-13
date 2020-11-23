4b21d1ecfee7f591937fa2cada4ac3626bc77377
Cameron Logie <cameronlogie24@gmail.com>
2020-11-23 16:12:11 +0000

    finished create method for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,13 +1,36 @@
 module Api
   module V1
     class PostsController < ApiController
+      before_action :check_basic_auth
+
       def index
         @posts = Post.order(created_at: :desc)
-        # @posts = Post.all.paginate(page: params[:page]) (page of posts)
-        # install will_paginate gem 
       end
       def show
         @post = Post.find(params[:id])
+      end
+      def create
+        p post_params["message"]
+        p @current_user.id
+        @post = Post.create(message: post_params["message"], user_id: @current_user.id)
+        
+        
+        # if @post
+        #   render json: @post
+        # else
+        #   render error: { error: 'Unable to create post.' }, status: 400
+        # end
+      end
+      def update
+
+      end
+
+
+      private 
+
+      def post_params
+        params.require(:message)
+        params.permit(:message)
       end
     end
   end
