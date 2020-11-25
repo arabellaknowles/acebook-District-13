a5447fac0436c07fe77bc2f7d9d8773058fdb5bb
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-25 17:48:14 +0000

    Tested & passed: User controller create method returns error if password is under 6 or over 10 characters

--- spec/api/v1/controllers/users_controller_spec.rb
+++ spec/api/v1/controllers/users_controller_spec.rb
@@ -74,4 +74,21 @@ RSpec.describe Api::V1::UsersController do
     end
   end
 
+  context "invallid password" do
+    it 'throws error if password is below 6 characters' do
+      user = create_user(username: "camlogie", full_name: "Cam Logie", email: "cam@gmail.com", password: "pass")
+      post_user(user)
+      expect(response).to have_http_status(:error)
+      json = JSON.parse(response.body)
+      expect(json['error']).to eq "Password invalid"
+    end
+
+    it 'throws error if password is over 10 characters' do
+      user = create_user(username: "camlogie", full_name: "Cam Logie", email: "cam@gmail.com", password: "passwordandpassword")
+      post_user(user)
+      expect(response).to have_http_status(:error)
+      json = JSON.parse(response.body)
+      expect(json['error']).to eq "Password invalid"
+    end
+  end
 end
