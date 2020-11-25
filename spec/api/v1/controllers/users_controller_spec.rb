12a158509fa7ac6251e90e7ae1c6bda2c5d6269b
Adrian.T <t.adrian88@gmail.com>
2020-11-25 16:07:58 +0000

    Added non-unique email test.

--- spec/api/v1/controllers/users_controller_spec.rb
+++ spec/api/v1/controllers/users_controller_spec.rb
@@ -44,6 +44,20 @@ RSpec.describe Api::V1::UsersController do
         expect(json['error']).to eq "Username in use"
       end
     end
+
+    context "non-unique email" do
+      before do
+        user = create_user(username: "camlogie", full_name: "Cam Logie", email: "cam@gmail.com", password: "password")
+        post_user(user)
+        user = create_user(username: "adrtoth", full_name: "Adrian Toth", email: "cam@gmail.com", password: "password3")
+        post_user(user)
+      end
+      it 'returns error' do
+        expect(response).to have_http_status(:error)
+        json = JSON.parse(response.body)
+        expect(json['error']).to eq "Email in use"
+      end
+    end
   end
 
 end
