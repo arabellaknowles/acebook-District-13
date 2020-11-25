90174e99edaf7e0c939a9b1adedfb0898c4cff84
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-25 17:43:35 +0000

    API users controller create method throws error if both username and email are not unique

--- spec/api/v1/controllers/users_controller_spec.rb
+++ spec/api/v1/controllers/users_controller_spec.rb
@@ -58,6 +58,20 @@ RSpec.describe Api::V1::UsersController do
         expect(json['error']).to eq "Email in use"
       end
     end
+
+    context "non-unique email and username" do
+      before do
+        user = create_user(username: "camlogie", full_name: "Cam Logie", email: "cam@gmail.com", password: "password")
+        post_user(user)
+        user = create_user(username: "camlogie", full_name: "Adrian Toth", email: "cam@gmail.com", password: "password3")
+        post_user(user)
+      end
+      it 'returns error' do
+        expect(response).to have_http_status(:error)
+        json = JSON.parse(response.body)
+        expect(json['error']).to eq "Username and email in use"
+      end
+    end
   end
 
 end
