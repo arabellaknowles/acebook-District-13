fe958142fb67f116f3f7212e7f8572573c3b4ab4
Cameron Logie <cameronlogie24@gmail.com>
2020-11-25 17:37:49 +0000

    not sure

--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Api::V1::PostsController do
       full_name: "A Dog", 
       password: "adoggle",
       id: 1)  
+
       allow_any_instance_of(Api::V1::PostsController).to receive(:check_basic_auth).and_return(post_user)
       allow_any_instance_of(Api::V1::PostsController).to receive(:current_user).and_return(post_user)
       post '/api/v1/posts', params: { message: "hello" }
@@ -18,6 +19,10 @@ RSpec.describe Api::V1::PostsController do
     it 'returns the post' do
       json = JSON.parse(response.body)
       expect(json["message"]).to eq("hello")
+    end
+
+    it 'returns a created status' do
+      expect(response).to have_http_status(:created)
     end
   end
 end
