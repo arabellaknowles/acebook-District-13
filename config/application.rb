820117ee0f7eace6b2b0cfe09d317a5112f45bed
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-26 23:52:25 +0000

    added all the routes requested from frontend

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Acebook
          origins 'localhost:3000'
          resource '*', 
                     :headers => :any, 
-                    :methods => [:get, :post, :options, :delete], 
+                    :methods => [:get, :post, :options, :delete, :put], 
                     :credentials => true
        end
     end
